1ebb574d569ad042af30e82d530d6a45bcdb2729
Volodymyr Rudov-Tsymbalist <uatyroni@air-volodymyr.home>
2023-04-04 18:11:11 +0200

    volodymyr/CON-13/Project-connect-to-firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { app, analytics } from "./firebase";
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  console.log(app)
   return (
     <div className="App">
       <div>
